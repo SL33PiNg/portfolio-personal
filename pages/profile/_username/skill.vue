d5e5798b6fe6b584a768f77e27c2e1fad91e331b
artzaba123 <Chanon_2008Rewsaki@hotmail.com>
2020-02-20 00:32:38 +0700

    เพิ่มเนื้อหา

--- pages/profile/_username/skill.vue
+++ pages/profile/_username/skill.vue
@@ -1,7 +1,19 @@
 <template>
-  <v-layout>
-    skill
-  </v-layout>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <p class="text-justify">
+          Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut,
+          faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus
+          adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut,
+          mollis sed, nonummy id, metus. Nulla consequat massa quis enim.
+          Praesent venenatis metus at tortor pulvinar varius. Donec venenatis
+          vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut
+          neque.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
