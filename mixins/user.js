eec1bf0ad9e1bca439fd6a54fc7126beb3a65afe
SL33PiNg <nook3920@gmail.com>
2020-02-20 19:35:34 +0700

    refac getUser()

--- mixins/user.js
+++ mixins/user.js
@@ -3,19 +3,22 @@ export default {
   data: () => ({
     loading: false
   }),
-  async created () {
-    this.loading = true
-    try {
-      const user = await this.$axios.$get('/users')
-      this.user = user
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error)
-    } finally {
-      this.loading = false
-    }
+  created () {
+    this.getUser()
   },
   methods: {
+    async getUser () {
+      this.loading = true
+      try {
+        const user = await this.$axios.$get('/users')
+        this.user = user
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error)
+      } finally {
+        this.loading = false
+      }
+    },
     async updateUser () {
       try {
         const user = await this.$axios.$patch('/users', {
