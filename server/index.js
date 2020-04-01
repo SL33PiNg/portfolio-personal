4743920087a366853c112b9178698d5e5ba3cb71
SL33PiNg <nook3920@gmail.com>
2020-04-01 21:04:35 +0700

    test froala upload image

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const morgan = require('morgan')
 const cors = require('cors')
 const express = require('express')
-
+const FroalaEditor = require('wysiwyg-editor-node-sdk/lib/froalaEditor')
 mongoose.connect('mongodb://localhost:27017/rmutt', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +18,7 @@ const db = mongoose.connection
 db.on('error', () => {
   console.log('connect error')
 })
-db.once('open', function() {
+db.once('open', function () {
   console.log('db connected')
 })
 
@@ -29,17 +29,24 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use('/', indexRouter)
 app.use('/avatar', express.static('./avatar'))
-app.use(function(err, req, res, next) {
+app.use('/report', express.static('./report'))
+
+app.post('/upload_image', (req, res) => {
+  FroalaEditor.Image.upload(req, '/report/', (err, data) => {
+    if (err) {
+      return res.send(JSON.stringify(err))
+    }
+    res.send(data)
+  })
+})
+
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = err
 
   // add this line to include winston logging
-  winston.error(
-    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
-    req.method
-    } - ${req.ip}`
-  )
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
 
   // render the error page
   res.sendStatus(err.status || 500)
